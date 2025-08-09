4c1cb18a65f22183070531e341a524edb1c0467c
Shikhar Yadav <92710243+shikharyadav16@users.noreply.github.com>
2025-08-09 10:13:10 +0530

    Update Connection.js

--- Connection.js
+++ Connection.js
@@ -1,8 +1,11 @@
+require('dotenv').config();  // load .env variables
+
 const mongoose = require("mongoose");
 
 async function connectToDb() {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/aidb");
+        const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/aidb";
+        await mongoose.connect(uri);
         console.log("Database connected!");
     } catch (err) {
         console.log("Error:", err);
